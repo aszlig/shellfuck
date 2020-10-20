146c6c92b16c2a5eb46cb68e5d2a33be244cb5d1
TpaeFawzen <30316430+GH-TpaeFawzen@users.noreply.github.com>
2020-10-20 20:45:26 +0900

    reduced if-fi and test -eq.

--- bf.sh
+++ bf.sh
@@ -16,7 +16,7 @@ bf_get()
 
     for i in $P_ARRAY;
     do
-        [ $iter -eq $P_APOS ] && echo "$i";
+        case $iter in ($P_APOS) echo "$i";; esac;
         iter=$(($iter + 1));
     done;
 }
@@ -27,20 +27,19 @@ bf_manip()
     P_ARRAY="$2";
     P_APOS="$3";
 
-    if [ "x$P_ACTION" = "x*" ];
-    then
-        CHANGED=$4;
-    else
-        CHANGED=$(($(bf_get "$P_ARRAY" "$P_APOS") $P_ACTION 1));
-        [ $CHANGED -lt 0 ] && CHANGED=255;
-        [ $CHANGED -gt 255 ] && CHANGED=0;
-    fi;
+    case "$P_ACTION" in
+        (\*) CHANGED=$4;;
+        (*)
+            CHANGED=$(($(bf_get "$P_ARRAY" "$P_APOS") $P_ACTION 1));
+            [ $CHANGED -lt 0 ] && CHANGED=255;
+            [ $CHANGED -gt 255 ] && CHANGED=0;;
+    esac;
 
     iter=0;
 
     for i in $P_ARRAY;
     do
-        [ $iter -eq $P_APOS ] && echo -n "$CHANGED " || echo -n "$i ";
+        case $iter in ($P_APOS) printf "$CHANGED ";; (*) "$i ";; esac;
         iter=$(($iter + 1));
     done;
 }
@@ -50,12 +49,10 @@ bf_fix()
     P_ARRAY="$1";
     P_APOS="$2";
 
-    if [ "x$(bf_get "$P_ARRAY" "$P_APOS")" != "x" ];
-    then
-        echo "$P_ARRAY";
-    else
-        echo "$P_ARRAY 0";
-    fi;
+    case "$(bf_get "$P_ARRAY" "$P_APOS")" in
+        ('') echo "$P_ARRAY 0";;
+        (*)  echo "$P_ARRAY";;
+    esac;
 }
 
 bf_d2a()
@@ -70,31 +67,31 @@ brainfuck()
     CODE="$(echo "$1" | sed 's/./& /g')";
     INSIDE_LOOP="$2";
 
-    if [ "x$INSIDE_LOOP" = "x" ];
-    then
-        [ $DEBUG -gt 0 ] && echo -en "\e[2J\e[2;0f";
-        ARRAY="0";
-        APOS=0;
-    else
-        ARRAY="$3";
-        APOS=$4;
-    fi;
+    case "$INSIDE_LOOP" in
+        ('')
+            [ "$DEBUG" -gt 0 ] && printf '\e[2J\e[2;0f';
+            ARRAY="0";
+            APOS=0;;
+        (*)
+            ARRAY="$3";
+            APOS=$4;;
+    esac;
 
     COLLECT=0;
     COLLECTION="";
 
     for cmd in $CODE;
     do
-        [ $COLLECT -eq 1 -a "x$cmd" != "x]" ] && COLLECTION="$COLLECTION$cmd";
+        case "$COLLECT:$cmd" in (1:) COLLECTION="$COLLECTION$cmd";; esac;
         case "$cmd" in
-            \<) APOS=$(($APOS - 1));;
-            \>) APOS=$(($APOS + 1));;
-            +|-) ARRAY="$(bf_manip "$cmd" "$ARRAY" "$APOS")";;
-            ,) read -n 1 C_CHAR; ARRAY="$(bf_manip "*" "$ARRAY" "$APOS" "$(printf '   %d\n' "'$C_CHAR")")";;
-            .) echo -n "$(bf_get "$ARRAY" "$APOS" | bf_d2a)";;
-            \[) COLLECT=1;;
-            \]) # special case: the LOOP, WOHOOO ;-)
-                [ $COLLECT -eq 0 ] && continue;
+            (\<) APOS=$(($APOS - 1));;
+            (\>) APOS=$(($APOS + 1));;
+            (+|-) ARRAY="$(bf_manip "$cmd" "$ARRAY" "$APOS")";;
+            (,) read -n 1 C_CHAR; ARRAY="$(bf_manip "*" "$ARRAY" "$APOS" "$(printf '   %d\n' "'$C_CHAR")")";;
+            (.) printf "$(bf_get "$ARRAY" "$APOS" | bf_d2a)";;
+            (\[) COLLECT=1;;
+            (\]) # special case: the LOOP, WOHOOO ;-)
+                case $COLLECT in (0) continue;; esac;
                 while [ "x$(bf_get "$ARRAY" "$APOS")" != "x0" ];
                 do
                     ARRAY="$(brainfuck "$COLLECTION" l "$ARRAY" "$APOS")";
@@ -109,16 +106,17 @@ brainfuck()
         [ $APOS -lt 0 ] && APOS=0;
 
         [ $DEBUG -gt 0 ] &&
-            echo -en "\e7\e[0;0f\e[1;31mcommand $cmd: $ARRAY ($APOS)\e[0m\e8" >&2;
+            printf "\e7\e[0;0f\e[1;31mcommand $cmd: $ARRAY ($APOS)\e[0m\e8" >&2;
 
         ARRAY="$(bf_fix "$ARRAY" "$APOS")";
     done;
 
-    if [ "x$INSIDE_LOOP" != "x" ];
-    then
-        echo "$ARRAY";
-        return $APOS;
-    fi;
+    case "$INSIDE_LOOP" in
+        ('') :;;
+        (*)
+            echo "$ARRAY";
+            return $APOS;;
+    esac;
 }
 
 # this actually is a brainfuck interpreter in shellscript (and probably
